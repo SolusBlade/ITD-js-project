5b46642e9059e9fa70244bb5ce5a128cdb64c38a
Hanna Bazdyrieva <119747532+Hanna-Bazdyrieva@users.noreply.github.com>
2023-03-13 21:25:42 +0100

    add modal-ingredient html and scss  + link hover in modal-recipe

--- src/js/header-search.js
+++ src/js/header-search.js
@@ -1,28 +1,28 @@
-import base from "./base-url";
+import base from './base-url';
 import axios from 'axios';
 
 const searfchFormRef = document.querySelector('.header__search');
 const searchInputRef = document.querySelector('.header__input');
 
 async function searchFetch(name) {
-    const url = `${base}search.php?s=${name}`;
-    try {
-        const {data} = await axios.get(`${url}`);
-        return data;
-      } catch ({response:{status}}) {
-        return status;
-      }
+  const url = `${base}search.php?s=${name}`;
+  try {
+    const { data } = await axios.get(`${url}`);
+    return data;
+  } catch ({ response: { status } }) {
+    return status;
+  }
 }
 
 searfchFormRef.addEventListener('submit', onSubmit);
 
 async function onSubmit(e) {
-    e.preventDefault();
-    
-    const drinks = await searchFetch(searchInputRef.value.trim());
-    console.log(drinks.drinks);
-    
-    // buildCard();
+  e.preventDefault();
+
+  const drinks = await searchFetch(searchInputRef.value.trim());
+  console.log(drinks.drinks);
+
+  // buildCard();
 }
 
 // function buildCard(){
