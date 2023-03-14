ecd41db23b19b467af2c50bb802a02bf8ef2622a
SolusBlade <kykysha59fafa@gmail.com>
2023-03-14 23:55:34 +0200

    Update auth.js

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,7 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getStorage, ref, uploadString } from "firebase/storage";
 
 const profileBtn = document.querySelector('.header__profile-btn');
+const assBtn = document.querySelector('#F');
+
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBEyxsja9Xmbt99yqe3GAmeQbiTjDFO9Ss",
@@ -15,15 +19,29 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const authProf = getAuth(app);
+const storage = getStorage(app);
+const cocktailsRef = ref(storage, 'cocktails');
 
 async function getIn(){
     const provider = new GoogleAuthProvider();
     
     try{
-        const user = await signInWithPopup(authProf, provider)
+        const user = await signInWithPopup(authProf, provider);
         console.log(user);
     } catch(error) {
         console.log(error);
     }
 }
+
+async function onBtnClick(){
+    const message = 'This is my message.';
+    console.log(storage);
+    uploadString(cocktailsRef, message).then((snapshot) => {
+     console.log(snapshot);
+     
+    });
+}
+
+
 profileBtn.addEventListener("click", getIn);
+assBtn.addEventListener('click', onBtnClick);
