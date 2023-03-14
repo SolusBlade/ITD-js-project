269d131f57529313c4dfd0bf490e34f62079b6a0
SolusBlade <kykysha59fafa@gmail.com>
2023-03-14 14:59:28 +0200

    try to add auth

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './js/header-search';
 import './js/fetchData';
 import './js/renderCards';
 import './js/randomCoctails';
-import './js/favorites';
+// import './js/favorites';
 import './js/modal-ingredient';
+import './js/auth';
 
