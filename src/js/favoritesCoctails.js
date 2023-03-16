f43f52d9cc70c8968aa759b0f8887580f2b61462
Khashyrova <121459042+Khashyrova@users.noreply.github.com>
2023-03-16 17:59:17 +0100

    adds

--- src/js/favoritesCoctails.js
+++ src/js/favoritesCoctails.js
@@ -5,7 +5,8 @@ import * as icons from '../images/icons.svg';
 
 const storageData = new FavoritesList();
 const listCocktails = document.querySelector('.favorite-cocktails__list');
-const listIngridient = document.querySelector('.favorite-ingredients__list');
+const galeryFavorits = document.querySelector('.favorite-cocktails');
+galeryFavorits.addEventListener('click', onBtnClick);
 
 // Пустий список//
 
@@ -13,6 +14,7 @@ function renderListCocktail() {
   const message = `<p class="favorite-cocktails__text">
           You haven't added any favorite cocktails yet
         </p>`;
+
   listCocktails.insertAdjacentHTML('beforebegin', message);
 }
 
@@ -28,6 +30,7 @@ async function fetchDataCoctail(id) {
 //Перевірка на пустий localStorage//
 if (storageData.cocktails.length === 0) {
   renderListCocktail();
+  galeryFavorits.removeEventListener('click', onBtnClick);
 }
 apiForIdCoctail();
 //Запит по id на коктель//
@@ -51,13 +54,20 @@ async function apiForIdCoctail() {
 //Видалити з localStorage//
 function onBtnClick(event) {
   const cocktailId = event.target.dataset.id;
-  if (storageData.cocktails.includes(cocktailId)) {
+  if (storageData.cocktails.includes(cocktailId))
     storageData.removeCocktail(cocktailId);
-  } else if (!storageData.cocktails) {
+  document
+    .querySelector(`[data-id = '${cocktailId}']`)
+    .closest('.favorite-cocktails__item')
+    .remove();
+  if (storageData.cocktails.length === 0) {
     renderListCocktail();
+    galeryFavorits.removeEventListener('click', onBtnClick);
   }
   return;
 }
+// renderListCocktail();
+
 //Рендер картки//
 function createCardCoctail(data) {
   let card = data
@@ -84,68 +94,4 @@ function createCardCoctail(data) {
     .join('');
 
   listCocktails.insertAdjacentHTML('beforeend', card);
-  const galleryFavorits = document.querySelector('.favorite-cocktails');
-  galleryFavorits.addEventListener('click', onBtnClick);
-}
-
-if (storageData.ingredients.length === 0) {
-  renderListIngredient();
-}
-apiForIdIngredient();
-
-function renderListIngredient() {
-  const message = `  <p class="favorite-ingredients__text">
-          You haven't added any favorite ingridients yet
-      </p>`;
-
-  listIngridient.insertAdjacentHTML('beforbegin', message);
-}
-
-async function apiForIdIngredient() {
-  try {
-    const ingredients = storageData.ingredients;
-
-    const ingredient = await Promise.all(
-      ingredients.map(async id => {
-        return await fetchDataCoctail(id);
-      })
-    );
-    if (!storageData.ingredients) {
-      renderListCocktail();
-    }
-    return createCardIngridient(ingredient);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-function createCardIngridient(data) {
-  let card = data
-    .map(item => {
-      return `<li class="favorite-ingredients__item">
-            <h2 class="favorite-ingredients__item-title">${item.strDrink}</h2>
-            <p class="favorite-ingredients__item-text">Liqueur</p>
-            <div class="favorite-button-container">
-              <button type="button" class="favorite-button__learn-more">
-                Learn more
-              </button>
-              <button type="button" class="favorite-button__remove">
-                Remove
-                <svg class="favorite-button__icon" width="17" height="15">
-                  <use href="./images/icons.svg#icon-full-heart"></use>
-                </svg>
-              </button>
-            </div>
-          </li>
-  `;
-    })
-    .join('');
-  if(listIngridient){
-    listIngridient.insertAdjacentHTML('beforeend', card);
-  }
-  const galleryFavorits = document.querySelector('.favorite-cocktails');
-  if(galleryFavorits){
-    galleryFavorits.addEventListener('click', onBtnClick);
-  }
-  
 }
