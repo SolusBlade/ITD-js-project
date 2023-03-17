ed5685fcb99ffb62e0b4ba67070fd94ccb394e7d
SolusBlade <kykysha59fafa@gmail.com>
2023-03-17 02:40:28 +0200

    a lot of fix 3

--- src/js/modals/modal-favorites-control.js
+++ src/js/modals/modal-favorites-control.js
@@ -1,15 +1,20 @@
 import StorageClass from '../favorites-storage-class';
 const storage = new StorageClass();
 
+import * as icons from '../../images/icons.svg';
+
 function onModalRecipeBtnClick(event) {
+  const parentBtn = document.querySelectorAll(`[data-id='${event.target.dataset.id}']`)
   const targetBtn = event.target;
   const cocktailId = event.target.dataset.id;
   if (storage.cocktails.includes(cocktailId)) {
     onRemoveBtn(targetBtn);
+    onRemoveGalleryBtn(parentBtn[1]);
     storage.removeCocktail(cocktailId);
     return;
   }
   onAddBtn(targetBtn);
+  onAddGalleryBtn(parentBtn[1]);
   storage.addCocktail(cocktailId);
 }
 
@@ -28,7 +33,23 @@ function onModalIngredientBtnClick(event) {
 function onAddBtn(button) {
   button.textContent = `Remove from favorites`;
 }
+function onAddGalleryBtn(button) {
+  button.innerHTML = `
+  Remove
+  <svg class="empty__heart" width="21" height="19">
+      <use href="${icons}#icon-full-heart"></use>
+  </svg>
+`;
+}
 function onRemoveBtn(button) {
   button.textContent = `Add to favorites`;
+}
+function onRemoveGalleryBtn(button) {
+  button.innerHTML = `
+  Add to
+  <svg class="empty__heart" width="21" height="19">
+      <use href="${icons}#icon-empty-heart"></use>
+  </svg>
+`;
 }
 export { onModalRecipeBtnClick, onModalIngredientBtnClick };
